162987015bd56fdafaf12602d95d2ed12e952571
Qazaroth <zakirullah03@gmail.com>
2020-05-12 12:45:40 +0800

    Added config file

--- pget.py
+++ pget.py
@@ -1,13 +1,39 @@
 from pathlib import  Path
-from bin import download
+from bin.download import Downloader
 
-url = "https://raw.githubusercontent.com/Qazaroth/pget-list/master/test.py"
+binDir = "./bin"
+scriptsDir = "./scripts"
 
-file_name = url.split("/")[-1]
-dir = "./scripts/{f}".format(f=file_name)
+configDir = "{b}/config.pget".format(b=binDir)
 
-f = Path(dir)
 
-print("Beginning file download...")
+def init():
+    print("Checking if config.pget exists...")
+    configFile = Path(configDir)
 
-download.Downloader.downloadScript(url)
+    if not configFile.is_file():
+        print("config.pget does not exist... making one...")
+        configFile = open(configDir, "w+")
+        configFile.write("[DO NOT DELETE]")
+        print("Made config.pget. Do not delete this file!")
+    else:
+        print("config.pget exist! Do not delete this file!")
+
+
+
+
+def main():
+    init()
+
+main()
+
+# url = "https://raw.githubusercontent.com/Qazaroth/pget-list/master/test.py"
+
+# file_name = url.split("/")[-1]
+# dir = "{s}/{f}".format(s=scriptsDir, f=file_name)
+
+# f = Path(dir)
+
+# print("Beginning file download...")
+
+# Downloader.downloadScript(url)
