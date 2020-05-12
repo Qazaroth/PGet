346b6eada426cb3291bb1b87da42d1b2f5c2593a
Qazaroth <realthedarksid3r@gmail.com>
2020-05-12 13:43:41 +0800

    Added version.txt and some minor changes made

--- update/update.py
+++ update/update.py
@@ -1,2 +1,5 @@
 upgrade_Hash_Location = "https://raw.githubusercontent.com/Qazaroth/PGet/master/bin/hash.txt"
-upgrade_Script_Location = ""
+upgrade_Script_Location = "https://raw.githubusercontent.com/Qazaroth/PGet/master/update/update.py"
+pget_location = "https://raw.githubusercontent.com/Qazaroth/PGet/master/pget.py"
+readme_location = "https://raw.githubusercontent.com/Qazaroth/PGet/master/README.md"
+
