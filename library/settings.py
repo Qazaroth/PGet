0f5291679eb947c33adde826a78f11d5b3237a2b
Qazaroth <realthedarksid3r@gmail.com>
2022-07-15 15:04:23 +0800

    v2.4.1 With necessary files Part 2

---
+++ library/settings.py
@@ -0,0 +1,18 @@
+# DO NOT TOUCH
+
+debugMode = False
+
+commandsFolder = "./commands"
+
+binDir = "./bin"
+scriptsDir = "./scripts"
+tempDir = "./temp"
+
+configDir = "{b}/config.pget".format(b=binDir)
+hashDir = "{b}/hash.txt".format(b=binDir)
+scriptListDir = "{b}/list.txt".format(b=scriptsDir)
+
+updateBat = "updater.bat"
+
+upgrade_Hash_Location = "https://raw.githubusercontent.com/Qazaroth/PGet/master/bin/hash.txt"
+script_List_Location = "https://raw.githubusercontent.com/Qazaroth/pget-list/master/master.txt"
